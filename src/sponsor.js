8d14886686f71c98d362b633f6a063ffccde3426
Allen Wang <abmathcounts@gmail.com>
2020-06-22 12:04:06 -0400

    prevent color change before sponsor sign in

--- src/sponsor.js
+++ src/sponsor.js
@@ -1,6 +1,4 @@
 import './styles/index.scss'
-import homeBackground from './images/home.png'
-import drwBackground from './images/drw.png'
 import axios from 'axios'
 
 const sponsorBaseURL = "http://localhost:8080/sponsor/";
@@ -9,8 +7,8 @@ var id = "";
 var signInForm = document.getElementById("sign-in-form");
 function handleSignIn(event) { 
     event.preventDefault();
-    id = document.getElementById("id").value;
-    axios.get(sponsorBaseURL + id).then((res) => {
+    axios.get(sponsorBaseURL + document.getElementById("id").value).then((res) => {
+        id = document.getElementById("id").value;
         document.getElementById("data-dump").innerHTML = res.data;
         document.getElementById("sponsor-name").innerHTML = res.data.Name + " admin page";
         document.getElementById("secret-id").innerHTML = "Secret id: " + res.data.Id;
@@ -22,17 +20,15 @@ signInForm.addEventListener('submit', handleSignIn);
 var colorForm = document.getElementById("change-color");
 function handleColorChange(event) { 
     event.preventDefault();
-    var color = document.getElementById("new-color").value;
-    console.log(color)
-    console.log(id)
-    axios.put(sponsorBaseURL + id, {
-        "color": color
-    }).then((res) => {
-        document.getElementById("room-color").innerHTML = "Room color: " + color;
-    });
+    if (id != "") {
+        var color = document.getElementById("new-color").value;
+        axios.put(sponsorBaseURL + id, {
+            "color": color
+        }).then((res) => {
+            document.getElementById("room-color").innerHTML = "Room color: " + color;
+        });
+    } else {
+        alert("Sign in first");
+    }
 } 
 colorForm.addEventListener('submit', handleColorChange);
-
-
-
-
