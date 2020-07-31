97d67f079f3ce36fb3d4af9749dd76836d553c3c
Mindy Long <mindyflong@gmail.com>
2020-07-31 14:23:09 -0700

    enabled browser notification options

--- src/register.js
+++ src/register.js
@@ -2,6 +2,7 @@ import './styles/register.scss'
 
 document.getElementById("SMS").addEventListener("click", enterPhone);
 document.getElementById("submit-button").addEventListener("click", redirect);
+// document.getElementById("browser").addEventListener("click", testNotifications);
 
 let userType = "hacker"
 toggleMode(userType)
@@ -46,6 +47,9 @@ function redirect() {
         textdata = [true, document.getElementById("phoneNum").value];
     }
 
+    // check if browser notifications are supported
+    setNotifications()
+
     localStorage.setItem('email', JSON.parse(email.checked));
     localStorage.setItem('phone', JSON.stringify(textdata));
     localStorage.setItem('browser', JSON.parse(browser.checked));
@@ -53,4 +57,33 @@ function redirect() {
     document.getElementById("submit-button").onclick = function () {
         window.location.href = "http://localhost:3000/character";
     }
+}
+
+/*
+WARNING: in chrome, user must go to "chrome://flags" and disable 'Enable native notifications'
+for this to work
+*/
+
+function setNotifications() {
+
+    if (!("Notification" in window)) {
+        console.log('This browser does not support desktop notification');
+        alert("This browser does not support desktop notification");
+    }
+    
+    else if (Notification.permission === "granted") {
+        let notification = new Notification("Browser notifications enabled!");
+        console.log("Browser notifications enabled!");
+    }
+    
+    else if (Notification.permission !== "granted") {
+        console.log("Browser notifications denied!");
+        Notification.requestPermission().then(function (permission) {
+          if (permission === "granted") {
+            let notification = new Notification("Hi there!");
+            console.log(notification)
+          }
+        });
+    }
+
 }
