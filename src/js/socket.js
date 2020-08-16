030f481d49e7fe56131b16d139f0934060a67bfe
Jack Cook <hello@jackcook.com>
2020-08-16 12:38:33 -0600

    Socket updates

--- src/js/socket.js
+++ src/js/socket.js
@@ -1,6 +1,7 @@
 class Socket {
   constructor() {
     this.conn = null;
+    this.subscribers = new Map();
   }
 
   send(data) {
@@ -11,8 +12,29 @@ class Socket {
     this.conn = new WebSocket('ws://localhost:8080/ws');
     this.conn.onopen = this.onopen;
     this.conn.onclose = this.onclose;
-    this.conn.onmessage = this.onmessage;
+    this.conn.onmessage = (e) => {
+      const messages = e.data.split('\n');
+
+      messages.forEach((msg) => {
+        const data = JSON.parse(msg);
+
+        this.subscribers.forEach((handler, channels) => {
+          if (channels.includes(data.type) || channels[0] === '*') {
+            handler(data);
+          }
+        });
+      });
+    };
   }
+
+  subscribe = (inputChannels, handler) => {
+    const channels =
+      typeof inputChannels === 'string' ? [inputChannels] : inputChannels;
+
+    channels.forEach((element) => {
+      this.subscribers.set(element, handler);
+    });
+  };
 }
 
 const socketInstance = new Socket();
