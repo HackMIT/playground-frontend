c2c582712f60860b57b9f290891b79fe40f87c3b
Kat <katherine.x.liu@gmail.com>
2020-08-22 22:50:38 -0500

    settings styling

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,7 +1,6 @@
 import './styles/settings.scss';
 import './images/settingsicon.svg';
 import './images/box.svg';
-import './images/box-filled.svg';
 import socket from './js/socket';
 
 // eslint-disable-next-line
@@ -42,17 +41,14 @@ class Settings {
     return (
       <div id="settings-text">
         <div id="settings-option">
-          {this.musicMuted ? (
-            <img id="settings-box" src="images/box-filled.svg" />
-          ) : (
-            <img id="settings-box" src="images/box.svg" />
-          )}
-          <button id="settings-music" onclick={this.handleMusicButton}>
+          <div id="settings-box"></div>
+          <button id="settings-button" onclick={this.handleMusicButton}>
             {this.musicMuted ? 'UNMUTE ALL MUSIC' : 'MUTE ALL MUSIC'}
           </button>
         </div>
-        <div>
-          <button id="settings-sound" onclick={this.handleSoundButton}>
+        <div id="settings-option">
+          <div id="settings-box"></div>
+          <button id="settings-button" onclick={this.handleSoundButton}>
             {this.soundMuted ? 'UNMUTE ALL SOUND' : 'MUTE ALL SOUND'}
           </button>
         </div>
@@ -64,16 +60,17 @@ class Settings {
     return (
       <div id="settings">
         <div id="root">
-          <div class="settings-page">
-            <div class="settings-header">
-              {/* <img class="settings-gear" src="images/settingsicon.svg" />
-                  <h1>SETTINGS</h1>
-                  <img class="settings-gear" src="images/settingsicon.svg" /> */}
-            </div>
-            <div class="settings-list" id="settings-list">
-              {this.createSettingsContent()}
-            </div>
-            <button class="settings-logout">LOGOUT</button>
+          <div class="settings-header" id="settings-header">
+            {/* <img class="settings-gear" src="images/settingsicon.svg" /> */}
+            <div id="settings-gear"></div>
+            <h1>SETTINGS</h1>
+            <div id="settings-gear"></div>
+          </div>
+          <div class="settings-list" id="settings-list">
+            {this.createSettingsContent()}
+          </div>
+          <div id="settings-flexcenter">
+            <button id="settings-logout">LOG OFF</button>
           </div>
         </div>
       </div>
