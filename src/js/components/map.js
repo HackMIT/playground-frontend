cb94485eebb405b3eee3487761f42a564b127b07
Jack Cook <hello@jackcook.com>
2020-09-09 16:55:58 -0400

    Basic map implemented

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,15 +1,34 @@
 import '../../styles/map.scss';
+
 // eslint-disable-next-line
 import socket from '../socket';
 
 // eslint-disable-next-line
 import createElement from '../../utils/jsxHelper';
 
+import '../../images/map/Ground.svg';
+import townSquare from '../../images/map/town_square.svg';
+
 class Map {
-
   createMapModal = () => {
+    return (
+      <div id="map">
+        <div id="map-container">
+          <button id="town-square" onclick={() => this.teleport('home')}>
+            <img src={townSquare} />
+          </button>
+        </div>
+      </div>
+    );
+  };
 
-    return <div id='map' />;
+  teleport = (room) => {
+    document.getElementById('modal-background').remove();
+
+    socket.send({
+      type: 'teleport',
+      to: room,
+    });
   };
 }
 
