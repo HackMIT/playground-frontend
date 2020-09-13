84f517c6949d54082df3e36efe285633fc0078d9
Jack Cook <hello@jackcook.com>
2020-09-13 12:07:33 -0400

    Updates

--- quillsso.699fbbb2a50f5d3a7280.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{186:function(o,n,t){o.exports=t(187)},187:function(o,n){var t=window.location.href.slice("http://localhost:3000/login?token=".length);window.location.href="".concat("http://localhost:3000","#").concat(t)}},[[186,0]]]);
