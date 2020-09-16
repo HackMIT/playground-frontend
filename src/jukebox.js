af682392d76ed69f08d482d0ef900d113b58b9e7
Julia Gonik <jgonik@mit.edu>
2020-09-15 23:28:59 -0700

    Fix scrolling issue but autoplay is wack

--- src/jukebox.js
+++ src/jukebox.js
@@ -11,32 +11,54 @@ import createElement from './utils/jsxHelper';
 class Jukebox {
   constructor() {
     this.songs = [];
-    this.currentSong = null; 
+    this.currentSong = null;
+    this.songStart = 0;
+    this.player = null;
+    this.ready = false;
 
-    socket.subscribe(['song', 'error', 'songs', 'playSong'], this.handleSocketMessage);
+    socket.subscribe(
+      ['song', 'error', 'songs', 'playSong'],
+      this.handleSocketMessage
+    );
   }
 
   handleSocketMessage = (msg) => {
     if (msg.type === 'songs') {
       this.songs = msg.songs;
-    }
-    else if (msg.type === 'playSong') {
-      this.currentSong = msg.song
-      this.updateJukeboxPane(msg.song);
-    }
-    else if (msg.type === 'song' && msg.remove) {
-      const index = this.songs.findIndex(x => x.id === msg.id);
+    } else if (msg.type === 'playSong') {
+      // this.currentSong = msg.song;
+      this.songStart = msg.start;
+      console.log(msg.song);
+      console.log(this.currentSong);
+      if (this.player !== null && this.ready) {
+        // song ended
+        // console.log(this.player.getPlayerState());
+        // while (this.player.getPlayerState() !== 0) {
+        //   console.log(this.player.getPlayerState());
+        // }
+        this.currentSong = msg.song;
+        console.log(this.player);
+        this.player.loadVideoById(msg.song.vidCode);
+        this.updateJukeboxPane(false);
+        this.playing = true;
+      } else {
+        this.currentSong = msg.song;
+        this.updateJukeboxPane(true);
+      }
+    } else if (msg.type === 'song' && msg.remove) {
+      const index = this.songs.findIndex((x) => x.id === msg.id);
       this.songs.splice(index, 1);
-      this.updateJukeboxPane();
-    }
-    else if (msg.type === 'song') {
+      this.updateJukeboxPane(false);
+    } else if (msg.type === 'song') {
       if (msg.requiresWarning) {
-        swal("Warning!",
-             "You will be disqualified from HackMIT 2020 if you submit any inappropriate songs or videos. Please visit go.hackmit.org/coc for more details about our code of conduct.",
-             "warning");
+        swal(
+          'Warning!',
+          'You will be disqualified from HackMIT 2020 if you submit any inappropriate songs or videos. Please visit go.hackmit.org/coc for more details about our code of conduct.',
+          'warning'
+        );
       }
       this.songs.push(msg);
-      this.updateJukeboxPane();
+      this.updateJukeboxPane(false);
     } else if (msg.type === 'error') {
       if (msg.code === 400) {
         swal('Oops!', 'Your song must be less than 6 minutes long.', 'error');
@@ -60,22 +82,24 @@ class Jukebox {
       return;
     }
 
-    const jukeboxBackgroundElem = document.getElementById('jukebox-background');
-    jukeboxBackgroundElem.classList.remove('opening');
-    jukeboxBackgroundElem.classList.add('closing');
+    const jukeboxElem = document.getElementById('jukebox-background');
+    jukeboxElem.classList.remove('opening');
+    jukeboxElem.classList.add('closing');
 
     setTimeout(() => {
-      jukeboxBackgroundElem.remove();
+      // jukeboxBackgroundElem.remove();
+      // jukeboxElem.style.display = 'none';
+      jukeboxElem.classList.remove('closing');
+      document.getElementById('hidden-jukebox').appendChild(jukeboxElem);
     }, 250);
   };
 
-  createPlayingNowContents = (song) => {
+  createPlayingNowContents = () => {
     let title;
-    if (song === undefined) {
-      title = "None";
-    }
-    else {
-      title = song.title;
+    if (this.currentSong.duration === 0) {
+      title = 'None';
+    } else {
+      title = this.currentSong.title;
     }
     return (
       <div>
@@ -86,98 +110,127 @@ class Jukebox {
   };
 
   createSongsQueueContents = () => {
-    const rootElem = <div />;
-
-    this.songs.forEach((song) => {
-      const minutesStr = Math.floor(song.duration / 60)
-        .toString()
-        .padStart(2, '0');
-
-      const secondsStr = (song.duration - Math.floor(song.duration / 60) * 60)
-        .toString()
-        .padStart(2, '0');
-      rootElem.appendChild(
-        <div className="jukebox-song">
-          <img src={song.thumbnailUrl} />
-          <div>
-            <p className="title">{song.title}</p>
-            <p className="duration">{`${minutesStr}:${secondsStr}`}</p>
-            <button id="remove" onclick={() => this.handleRemoveButton(song)}>Remove</button>
-          </div>
-        </div>
-      );
+    socket.send({
+      type: 'get_songs',
     });
+    const rootElem = <div />;
+    setTimeout(() => {
+      this.songs.forEach((song) => {
+        const minutesStr = Math.floor(song.duration / 60)
+          .toString()
+          .padStart(2, '0');
+
+        const secondsStr = (song.duration - Math.floor(song.duration / 60) * 60)
+          .toString()
+          .padStart(2, '0');
+        rootElem.appendChild(
+          <div className="jukebox-song">
+            <img src={song.thumbnailUrl} />
+            <div>
+              <p className="title">{song.title}</p>
+              <p className="duration">{`${minutesStr}:${secondsStr}`}</p>
+              <button id="remove" onclick={() => this.handleRemoveButton(song)}>
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      });
+    }, 250);
 
     return rootElem;
   };
 
   openJukeboxPane = (rootElem) => {
-    const jukeboxElem = (
-      <div id="jukebox-background" onclick={this.closeJukeboxPane}>
-        <div id="jukebox-container" className="Jukebox-queue-container">
-          <div id="jukebox-header">
-            <div className="spacer" />
-            <h2>Jukebox</h2>
-            <button id="jukebox-close-button" onclick={this.closeJukeboxPane}>
-              <img src={closeIcon} />
-            </button>
-          </div>
-          <div id="player" />
-          <div id="jukebox-playing-now"></div>
-          <div id="jukebox-songs-queue"></div>
-          <div id="jukebox-controls">
-            <p>Add a song to the queue:</p>
-            <div>
-              <input
-                id="jukebox-song-input"
-                type="text"
-                className="Jukebox-queue-container-controls-input"
-                placeholder="https://youtube.com/dQw4w9WgXcQ"
-              />
-              <button
-                id="jukebox-queue-button"
-                className="Jukebox-queue-container-controls-input-button"
-                onclick={this.handleSubmitButton}
-              >
-                Submit
+    let jukeboxElem = document.getElementById('jukebox-background');
+    if (jukeboxElem === null) {
+      jukeboxElem = (
+        <div id="jukebox-background" onclick={this.closeJukeboxPane}>
+          <div id="jukebox-container" className="Jukebox-queue-container">
+            <div id="jukebox-header">
+              <div className="spacer" />
+              <h2>Jukebox</h2>
+              <button id="jukebox-close-button" onclick={this.closeJukeboxPane}>
+                <img src={closeIcon} />
               </button>
+            </div>
+            <div id="player" />
+            <div id="jukebox-playing-now"></div>
+            <div id="jukebox-songs-queue"></div>
+            <div id="jukebox-controls">
+              <p>Add a song to the queue:</p>
+              <div>
+                <input
+                  id="jukebox-song-input"
+                  type="text"
+                  className="Jukebox-queue-container-controls-input"
+                  placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO"
+                />
+                <button
+                  id="jukebox-queue-button"
+                  className="Jukebox-queue-container-controls-input-button"
+                  onclick={this.handleSubmitButton}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
 
     rootElem.appendChild(jukeboxElem);
-    this.updateJukeboxPane();
-    jukeboxElem.classList.add('opening');
+    // jukeboxElem.style.display = 'flex';
 
-    this.updateYouTubePlayer(this.currentSong);
+    socket.send({
+      type: 'get_current_song',
+    });
+    socket.send({
+      type: 'get_songs',
+    });
+    setTimeout(() => {
+      jukeboxElem.classList.add('opening');
+      this.updateJukeboxPane(true);
+    }, 25);
+    // this.updateYouTubePlayer();
   };
 
-  updateYouTubePlayer = (song) => {
-
-    if (song === undefined || song === null) {
+  updateYouTubePlayer = () => {
+    if (this.currentSong.duration === 0) {
       return;
     }
 
+    console.log(this.currentSong);
+
     setTimeout(() => {
-
-    console.log(song);
-
-    YouTubeIframeLoader.load((YT) => {
-      this.player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: song.vidCode,
-        playerVars: {
-          autoplay: 1,
-          controls: 0,
-          start: song.start
-        },
+      YouTubeIframeLoader.load((YT) => {
+        this.player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: this.currentSong.vidCode,
+          playerVars: {
+            autoplay: 1,
+            controls: 0,
+            start: this.songStart,
+            loop: 1,
+            events: {
+              'onReady': this.onPlayerReady,
+            },
+          },
+        });
       });
-    });
-
     }, 1);
+
+    socket.send({
+      type: 'get_songs',
+    });
+    this.updateJukeboxPane(false);
+  };
+
+  onPlayerReady = (event) => {
+    console.log('we in here');
+    this.ready = true;
   };
 
   handleSubmitButton = () => {
@@ -198,8 +251,8 @@ class Jukebox {
       return;
     }
 
-    const splitUrl = url.search.split('=');
-    const vidCode = splitUrl[splitUrl.length - 1];
+    const splitUrl = url.search.split(/[&=]/);
+    const vidCode = splitUrl[splitUrl.length - 3];
 
     socket.send({
       type: 'song',
@@ -208,28 +261,26 @@ class Jukebox {
   };
 
   handleRemoveButton = (song) => {
-    const newSongPacket = { ...song};
+    const newSongPacket = { ...song };
     newSongPacket.remove = true;
     newSongPacket.type = 'song';
     socket.send(newSongPacket);
-  }
+  };
 
-  updateJukeboxPane = (song) => {
+  updateJukeboxPane = (updateVideo) => {
     // Get updated songs from queue
-    socket.send({
-      type: 'get_songs',
-    });
-
+    if (updateVideo) {
+      this.updateYouTubePlayer();
+    }
     document.getElementById('jukebox-playing-now').innerHTML = '';
     document
       .getElementById('jukebox-playing-now')
-      .appendChild(this.createPlayingNowContents(song));
+      .appendChild(this.createPlayingNowContents());
 
     document.getElementById('jukebox-songs-queue').innerHTML = '';
     document
       .getElementById('jukebox-songs-queue')
       .appendChild(this.createSongsQueueContents());
-    this.updateYouTubePlayer(this.currentSong);
   };
 }
 
