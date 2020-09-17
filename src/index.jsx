059b57a5249a2ee27d2823f1b2dde6d9490e403b
Natalie <natalie.huang99@gmail.com>
2020-09-16 23:29:45 -0700

    teleport friend + close modals

--- src/index.jsx
+++ src/index.jsx
@@ -217,6 +217,15 @@ class Game extends Page {
       this.scene.fixCameraOnResize();
       this.handleWindowSize();
     });
+
+    window.onclick = (e) => {
+      console.log(e.target.id);
+      if (e.target.id === "modal-background") {
+        document.getElementById('modal-background').remove();
+      } else if (e.target.id === 'form-modal-background') {
+        document.getElementById('form-modal-background').remove();
+      }
+    }
   };
 
   handleGameClick = (e) => {
--- src/js/components/nonprofit.js
+++ src/js/components/nonprofit.js
@@ -65,6 +65,32 @@ class NonprofitPopup {
             </ul>
           </div>
         );
+      case 'urban_displacement':
+        return (
+          <div id="nonprofit-popup">
+            <h1>Urban Displacement Project</h1>
+            <h3>Description</h3>
+            <p>
+              UDP conducts community-centered, data-driven, applied research toward more equitable and inclusive futures for cities. Our research aims to understand and describe the nature of gentrification and displacement, and also to generate knowledge on how policy interventions and investment can respond and support more equitable development.
+            </p>
+            <br></br>
+            <p>
+              The goal of UDP is to produce rigorous research and create tools to empower advocates and policymakers, to reframe conversations, and to train and inspire the next generation of leaders in equitable development.
+            </p>
+            <h3>Resources</h3>
+            <ul>
+              <li>
+                <a href="https://www.sensitivecommunities.org/">
+                  https://www.sensitivecommunities.org
+                </a>{' '}
+              </li>
+              <li>
+                Check out code on our github repo for identifying tract level locations of communities that are sensitive to displacement and housing cost increases
+              </li>
+            </ul>
+
+          </div>
+        );
       case 'kiva':
         return (
           <div id="nonprofit-popup">
